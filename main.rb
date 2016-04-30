fd540125de7633d182f03f3908c59d7a4152a219
Tony Contreras <tony.contreras@gmail.com>
2016-04-29 19:12:05 -0700

    album art for pandora and spotify

--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 require 'sonos'
 require 'pry'
 
 
-get '/' do 
-	erb	:home
+get '/' do
+  system = Sonos::System.new
+  speaker = system.speakers.first
+  @artist = speaker.now_playing[:artist]
 
+  if /spotify/ =~ speaker.now_playing[:uri]
+    @album_art_url = speaker.now_playing[:album_art]
+    @service = 'Spotify'
+
+  elsif /pandora/ =~ speaker.now_playing[:uri]
+    @album_art_url = speaker.now_playing[:album_art].gsub(/(^http:\/\/\d+.\d+.\d+.\d+\:\d+)/,'')
+    @service = 'Pandora'
+  end
+  erb	:home
 
 end

bbdc5381766339b2f57db2c324e0afe731cb623c
Tony Contreras <tony.contreras@gmail.com>
2016-04-30 18:41:11 -0700

    nope - still not working

--- public/js/sobud.js
+++ public/js/sobud.js
@@ -1,33 +1,33 @@
 $(function()  {
-  console.log("ready Freddy!");
+  console.log("Ready Freddy!");
   var xmlhttp = new XMLHttpRequest();
   var url = "http://localhost:4567/refresh.json";
-  window.currentSong = null;
-  window.lastSong = null;
+  var lastSong;
+  var currentSong;
+
 
   function getStatus() {
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-
     xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
           var speakerStatus = JSON.parse(xmlhttp.responseText);
-          window.currentSong = speakerStatus.uri;
+          currentSong = speakerStatus.uri;
           console.log(speakerStatus);
-          console.log(speakerStatus.status);
         }
     };
+  
   }
 
-  function refresh() {
-    if (window.lastSong != null && window.currentSong != window.lastSong) {
-      console.log('fire in the hole');
-    }
+  function refresher() {
+    lastSong   
   }
 
-  setInterval(getStatus,3000);
-  setInterval(refresh,3000);
-
+  setInterval(getStatus,10000);
+  setInterval(wtf, 5000) 
 
 });
+
+
+
 

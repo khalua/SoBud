0972ab306ffb0fd5d09470e80e715daf2415d6dd
Tony Contreras <tony.contreras@gmail.com>
2016-05-02 09:56:24 -0700

    i think refresh is working now

--- public/js/sobud.js
+++ public/js/sobud.js
@@ -12,7 +12,7 @@ $(function()  {
     xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
           speakerStatus = JSON.parse(xmlhttp.responseText);
-          if (song1 === undefined) {
+          if ( typeof song1 === undefined ) {
             song1 = speakerStatus.uri;
           }
           song2 = speakerStatus.uri;
@@ -22,9 +22,18 @@ $(function()  {
   }
 
   function refresh() {
-    if (song1 != undefined && song1 != song2) {
-        console.log("new song!");
+    console.log("song1: " + song1);
+    console.log("song2: " + song2);
+    if ( typeof song1 !== undefined && song1 != song2) {
+        console.log("new song! reload");
         location.reload();
+    }
+
+    if ( typeof song1 === undefined && typeof song2 !== undefined ) {
+      if (speakerStatus.status == 'playing') {
+        console.log("maybe new song, let's reload!");
+        location.reload();
+      }
     }
   }
 
